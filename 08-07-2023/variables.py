7e6bf253b094fd4d58581c8d017e4e64d3e4039b
Arun <rarunkumar385@gmail.com>
2023-07-08 19:22:22 +0530

    practise

---
+++ 08-07-2023/variables.py
@@ -0,0 +1,14 @@
+data=[1,2,3,4,13.5,True,2+3j]
+print(data)
+
+
+data1=(1,2,5,7,9,True)
+print(data1)
+
+
+data2={'name':'arun',
+       'date':'2-3-2023',
+       'location':'Bangalore',
+       }
+print(data2)
+
