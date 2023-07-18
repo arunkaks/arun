a4abf64fd62c4cf81ad1bae007365f50f7c56bbf
Arun <rarunkumar385@gmail.com>
2023-07-18 19:23:02 +0530

    commenting

---
+++ 08-07-2023/sample.py
@@ -0,0 +1,9 @@
+username='Arun'
+passr=12345
+
+name=str(input("Enter the name:"))
+password=int(input("Enter the password:"))
+if name==username and password == passr:
+    print("Sucess")
+else:
+    print("Invalid")
